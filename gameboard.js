9719d6d3bf5291b1117b312fa5ea13a9f3a6ec58
SzamanHerbaty <kuba.pietrasik1234@gmail.com>
2026-08-09 18:01:50 +0200

    stworzenie modułu gameboard

---
+++ gameboard.js
@@ -0,0 +1,66 @@
+export const gameboard = (() => {
+
+    let gameboardGrid = [0, 0, 0,
+        0, 0, 0,
+        0, 0, 0];
+
+    //  changeGridValues(index, player) - zmienia na planszy (tablica gameboardGrid) wartość pod podanym indexem 
+    //  w zależności jaki player zmienił wartość na planszy
+    //
+    // Jeżeli gracz z X to gameboardGrid[index] = 1;
+    // Jeżeli gracz z O to gameboardGrid[index] = 2;
+    const changeGridValues = (index, player) => {
+
+        if (gameboardGrid[index] !== 0) {
+            return "This index is already taken";
+        }
+
+        if (player.marker === "X") {
+            gameboardGrid[index] = 1;
+        }
+        else {
+            gameboardGrid[index] = 2;
+        }
+    };
+
+    const checkWin = () => {
+        // Lista wszystkich możliwych kombinacji które wygrywają kółko i krzyżyk
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Wiersze
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Kolumny
+            [0, 4, 8], [2, 4, 6] // Przekątne
+        ];
+
+        for (let combination of winningCombinations) {
+            const [a, b, c] = combination;
+
+            if (gameboardGrid[a] !== 0 && gameboardGrid[a] === gameboardGrid[b] && gameboardGrid[a] === gameboardGrid[c]) {
+                return 1;
+            }
+        }
+
+        if (!gameboardGrid.includes(0)) {
+            return 2;
+        }
+
+
+        return null;
+    };
+
+    const getGrid = () => {
+        return gameboardGrid;
+    };
+
+    const clearGrid = () => {
+        gameboardGrid = [0, 0, 0,
+            0, 0, 0,
+            0, 0, 0];
+    };
+
+    return {
+        changeGridValues,
+        checkWin,
+        getGrid,
+        clearGrid
+    };
+})();
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { gameboard } from "./gameboard";
 
 const domGrid = document.querySelectorAll(".game_board_item");
 const currentPlayer = document.querySelector(".current_player");
@@ -6,83 +7,20 @@ const restartBtn = document.querySelector(".restart_btn");
 
 const form = document.querySelector(".name_form");
 
-function createPlayer(name, marker){
-    if (marker !== "X" && marker !== "O"){
-        return "Enter X or O as a marker";
-    }
+class Player {
+    constructor(name, marker) {
+        if (marker !== "X" && marker !== "O") {
+            throw new Error("Enter X or O as a marker");
+        }
 
-    return {name, marker};
+        this.name = name;
+        this.marker = marker;
+    }
 }
 
-const gameboard = (() => {
-    
-    let gameboardGrid = [0, 0, 0, 
-                         0, 0, 0, 
-                         0, 0, 0];
-
-    //  changeGridValues(index, player) - zmienia na planszy (tablica gameboardGrid) wartość pod podanym indexem 
-    //  w zależności jaki player zmienił wartość na planszy
-    //
-    // Jeżeli gracz z X to gameboardGrid[index] = 1;
-    // Jeżeli gracz z O to gameboardGrid[index] = 2;
-
-    const changeGridValues = (index, player) =>{
-
-        if(gameboardGrid[index] !== 0){
-            return "This index is already taken";
-        }
-
-        if(player.marker === "X"){
-            gameboardGrid[index] = 1;
-        }
-        else{
-            gameboardGrid[index] = 2;
-        }
-    };
-
-    const checkWin = () =>{
-        // Lista wszystkich możliwych kombinacji które wygrywają kółko i krzyżyk
-
-        const winningCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Wiersze
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Kolumny
-            [0, 4, 8], [2, 4, 6]             // Przekątne
-        ];
-        
-        for (let combination of winningCombinations){
-            const [a, b, c] = combination;
-
-            if (gameboardGrid[a] !== 0 && gameboardGrid[a] === gameboardGrid[b] && gameboardGrid[a] === gameboardGrid[c]){
-                return 1;
-            }
-        }
-
-        if (!gameboardGrid.includes(0)){
-                return 2;
-        }
-
-
-        return null;
-    };
-
-    const getGrid = () => {
-        return gameboardGrid;
-    }
-
-    const clearGrid = () =>{
-        gameboardGrid = [0, 0, 0, 
-                         0, 0, 0, 
-                         0, 0, 0];
-    }
-
-    return {
-        changeGridValues,
-        checkWin,
-        getGrid,
-        clearGrid
-    }
-})();
-
+function createPlayer(name, marker){
+    return new Player(name, marker);
+}
 
 /**
  * Obiekt odpowiedzialny za kontrolę przebiegu gry.
